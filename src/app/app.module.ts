2c76c0cde5ace1f45161c4c8cf8af9bd1b46d083
ayaalsaid <140819543+ayaalsaid@users.noreply.github.com>
2023-11-06 23:06:23 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CoursesComponent } from './courses/courses.component';
+import { RatebarComponent } from './ratebar/ratebar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    CoursesComponent,
+    RatebarComponent,
+
+  
   ],
   imports: [
     BrowserModule,
